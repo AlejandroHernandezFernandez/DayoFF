9a7d00c605ee257241201b3319e94ab4de9e3d8a
adrian.reyes169 <adrian.reyes10@upr.edu>
2025-07-24 09:39:16 -0400

    Made a calendar which is adjusted to the current month and year

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import datetime 
+import calendar
 
 
 # Use storage for Passwords and Users  
@@ -7,6 +9,17 @@ Test_User = "user123"
 Test_Pass = "password123"
 Test_Admin_User = "admin"
 Test_Admin_Pass = "adminpass"
+
+today = datetime.date.today()
+months = ["January", "February", "March", 
+          "April", "May", "June", 
+          "July", "August", "September", 
+          "October", "November", "December"]
+
+current_month = today.month 
+current_year = today.year
+num_days = calendar.monthrange(current_year,current_month)[1]
+first_weekday = calendar.monthrange(current_year, current_month)[0]
 
 # Function to display the Login Screen 
 def login_screen():
@@ -36,9 +49,36 @@ def login_screen():
 
 #  Function to display the Main Application Screen 
 def main_app_screen():
-    st.title("Main Screen")
+    st.title("Day oFF Tracker!")
+    st.subheader(f"{months[current_month-1]}, {current_year}") # <- Prints the current month as a title based on the months list
     # We can add the rest of the app stuff here
-    #->
+    # Prints the days of the month as buttons
+    day = 1
+    row = 0
+    col1, col2, col3, col4 , col5, col6, col7 = st.columns(7)
+    cols = [col1, col2, col3, col4, col5, col6, col7]
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    
+    for i in range(7):
+        cols[i].markdown(f''':orange[{days[i]}]''')
+
+    while day < num_days:
+        for col in range(7):
+            if row == 0 and col < first_weekday:
+                with cols[col]:
+                    st.write("&nbsp;<br>" * 2, unsafe_allow_html=True)
+            else:
+                with cols[col]:
+                    if day <= num_days:
+                        st.button(str(day))
+                        day+=1
+                    else:
+                        st.write("")
+        row +=1 
+        
+
+
+    #-> We should later make these buttons be able to open a new screen where the info is displayed
     #->
     #->
 
